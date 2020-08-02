df8de2ae87d26770c40dd9cf62a9a45536a868e4
Alec <Quaintman2000@gmail.com>
2020-08-01 18:01:01 -0700

    worked on 3

--- Assets/Scripts/EnemySpawner.cs
+++ Assets/Scripts/EnemySpawner.cs
@@ -1,9 +1,13 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Threading;
 using UnityEngine;
 
 public class EnemySpawner : MonoBehaviour
 {
+    List<GameObject> waypoints = new List<GameObject>();
+    private GameObject spawnedEntity;
+    private int count = 0;
     // Start is called before the first frame update
     void Start()
     {
@@ -12,6 +16,11 @@ public class EnemySpawner : MonoBehaviour
 
     public void SpawnEnemy(GameObject enemy)
     {
-        Instantiate(enemy, this.gameObject.transform.position, this.gameObject.transform.rotation);
+        count = 0;
+        spawnedEntity=Instantiate(enemy, this.gameObject.transform.position, this.gameObject.transform.rotation);
+        for(int i =0; i<waypoints.Count; i++)
+        {
+            spawnedEntity.GetComponent<AiController>().waypoints[i] = waypoints[i].transform;
+        }
     }
 }
